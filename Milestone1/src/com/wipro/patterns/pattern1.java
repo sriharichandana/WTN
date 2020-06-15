70e9f40d41020be5d9ea9419d425f1831e7616b6
CHANDANA <sriharichandana.ch@gmail.com>
2020-06-15 13:20:19 +0530

    Pattern Exercises

---
+++ Milestone1/src/com/wipro/patterns/pattern1.java
@@ -0,0 +1,24 @@
+package com.wipro.patterns;
+
+    //****
+	//****
+	//****
+	//****
+
+public class pattern1 {
+		public static void main(String args[]) {
+			int i,j;
+			int n=Integer.parseInt(args[0]);
+			for(i=0;i<n;i++)
+			{
+				for(j=0;j<n;j++)
+				{
+					System.out.print("*");
+				}
+				System.out.println("");
+			}
+		}
+
+	}
+
+
