0d945c19663da1835269404ba9db601aa1f436c7
CHANDANA <sriharichandana.ch@gmail.com>
2020-06-15 10:14:08 +0530

    Language Basics Ex1

---
+++ Milestone1/src/com/wipro/languagebasics/Ex1.java
@@ -0,0 +1,9 @@
+package com.wipro.languagebasics;
+
+public class Ex1 {
+	public static void main(String args[]) {
+		System.out.print(args[0]+" Technologies ");
+		System.out.println(args[1]);
+	}
+
+}
