07cd27406715360d860b39f6d825f485a733029b
CHANDANA <sriharichandana.ch@gmail.com>
2020-06-15 10:12:39 +0530

    Flow Control Statements Ex10

---
+++ Milestone1/src/com/wipro/flowcontrolstatements/Ex10.java
@@ -0,0 +1,12 @@
+package com.wipro.flowcontrolstatements;
+
+public class Ex10 {
+public static void main(String args[]) {
+		
+		for (int i=1;i<=10;i++)
+		{
+			System.out.print(i+" ");
+		}
+	}
+
+}
