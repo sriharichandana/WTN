84dd9dc798339918bf520c210f59adedeee6142a
CHANDANA <sriharichandana.ch@gmail.com>
2020-06-15 10:10:48 +0530

    Flow Control Statements Ex2

---
+++ Milestone1/src/com/wipro/flowcontrolstatements/Ex2.java
@@ -0,0 +1,17 @@
+package com.wipro.flowcontrolstatements;
+
+public class Ex2 {
+	public static void main(String args[])
+	{
+		int a=Integer.parseInt(args[0]);
+		if(a%2==0)
+		{
+			System.out.println("Even Number");
+		}
+		else
+		{
+			System.out.println("Odd Number");
+		}
+	}
+
+}
