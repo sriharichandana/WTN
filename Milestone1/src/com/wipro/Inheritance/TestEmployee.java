180dd79566b9a73ea9db948977bbdea2389d8fa6
CHANDANA <sriharichandana.ch@gmail.com>
2020-06-24 23:00:41 +0530

    Inheritance

---
+++ Milestone1/src/com/wipro/Inheritance/TestEmployee.java
@@ -0,0 +1,12 @@
+package com.wipro.Inheritance;
+
+public class TestEmployee {
+	public static void main(String args[]) {
+		Person p=new Person("person1");
+		
+		Employee e=new Employee("sindhu",2015.5,2020,"45758ghbnm");
+		System.out.println(p);
+		System.out.println(e);
+	}
+
+}
