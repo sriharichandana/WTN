78834f5aa438208a134087be5de91dd9d1e0ccb6
CHANDANA <sriharichandana.ch@gmail.com>
2020-06-24 23:00:29 +0530

    Interfaces

---
+++ Milestone1/src/com/wipro/interfaces/music/wind/Saxophone.java
@@ -0,0 +1,9 @@
+package com.wipro.interfaces.music.wind;
+import com.wipro.interfaces.music.Playable;
+public class Saxophone implements Playable {
+	public void play()
+	{
+		System.out.println("This is Saxophone from music.wind");
+	}
+
+}
