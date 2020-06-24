24151b89c1ee4813163591ae7a82d4a7b87767db
CHANDANA <sriharichandana.ch@gmail.com>
2020-06-24 23:00:03 +0530

    Interfaces

---
+++ Milestone1/src/com/wipro/interfaces/music/string/Veena.java
@@ -0,0 +1,9 @@
+package com.wipro.interfaces.music.string;
+import com.wipro.interfaces.music.Playable;
+public class Veena implements Playable{
+	public void play()
+	{
+		System.out.println("This is Veena from music.string");
+	}
+
+}
