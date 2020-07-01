a474cf1c45aeb2a78d12218160731d30c538b934
CHANDANA <sriharichandana.ch@gmail.com>
2020-07-01 07:00:16 +0530

    Exception Handling

---
+++ Milestone1/src/com/wipro/ExceptionHandling/Ex5/Exe5.java
@@ -0,0 +1,15 @@
+package com.wipro.ExceptionHandling.Ex5;
+
+public class Exe5 {
+	public static void main(String[] args) throws InvalidAgeException {
+		String name = args[0];
+		
+		int age = Integer.parseInt(args[1]);
+		
+		if (age < 18 || age >= 60)
+			throw new InvalidAgeException();
+		
+		System.out.println("Name: " + name + " Age: " + age);
+	}
+
+}
